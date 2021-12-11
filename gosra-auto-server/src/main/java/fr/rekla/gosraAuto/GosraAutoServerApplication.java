c286477848a8838ccb92e97d186266e4bb6b52e9
rekla abderrahim <reklaabderrahim@hotmail.com>
2021-12-11 11:42:49 +0100

    first commit

---
+++ gosra-auto-server/src/main/java/fr/rekla/gosraAuto/GosraAutoServerApplication.java
@@ -0,0 +1,13 @@
+package fr.rekla.gosraAuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosraAutoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GosraAutoServerApplication.class, args);
+    }
+
+}
---
+++ gosra-auto-server/src/test/java/fr/rekla/gosraAuto/GosraAutoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.rekla.gosraAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosraAutoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
